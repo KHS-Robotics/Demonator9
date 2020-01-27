6284d46b8cf28e17110ac1c872e5ef8935f7b2f5
LukeS26 <jeremychernin@gmail.com>
2020-01-27 07:34:03 -0500

    Fixed translation2d signs

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,7 +15,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.DriveSwerveWithXbox;
 import frc.robot.commands.PivotPIDTuner;
 import frc.robot.subsystems.SwerveDrive;
-import frc.robot.subsystems.SwerveModule;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
