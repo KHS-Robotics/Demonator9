08bd219d3dfa4f8c682839613f669ce21e1985b8
LukeS26 <LukeS062022@gmail.com>
2020-03-07 22:53:11 -0500

    5 Ball Steal

--- src/main/java/frc/robot/AutoCommands.java
+++ src/main/java/frc/robot/AutoCommands.java
@@ -21,7 +21,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.commands.drive.rotate.RotateToAngle;
 import frc.robot.commands.drive.rotate.RotateToTarget;
 import frc.robot.commands.hood.AlignHoodToTarget;
 import frc.robot.commands.indexer.SetIndexerAuto;
@@ -29,23 +28,25 @@ import frc.robot.commands.shooter.RampShooter;
 import frc.robot.commands.shooter.ShootAuto;
 
 /**
- * Add your docs here.
+ * Class to generate commands for autonomous movement
  */
 public class AutoCommands {
-  public static SwerveControllerCommand wallLineUp, frontTrench, pickTrench, returnTrench, moveOffInit;
+  public static SwerveControllerCommand wallLineUp, frontTrench, pickTrench, returnTrench, moveOffInit, steal, moveFromSteal;
 
   public static void autoInit() {
-    wallLineUp = loadPathweaverTrajectory("output/WallLineUp.wpilib.json");
-    frontTrench = loadPathweaverTrajectory("output/FrontOffset.wpilib.json");
-    pickTrench = loadPathweaverTrajectory("output/PickTrench.wpilib.json");
-    returnTrench = loadPathweaverTrajectory("output/ReturnFromLastBall.wpilib.json");
-    moveOffInit = loadPathweaverTrajectory("output/MoveOffInit.wpilib.json");
+    wallLineUp = loadPathweaverTrajectory("WallLineUp");
+    frontTrench = loadPathweaverTrajectory("FrontOffset");
+    pickTrench = loadPathweaverTrajectory("PickTrench");
+    returnTrench = loadPathweaverTrajectory("ReturnFromLastBall");
+    moveOffInit = loadPathweaverTrajectory("MoveOffInit");
+    steal = loadPathweaverTrajectory("Steal");
+    moveFromSteal = loadPathweaverTrajectory("MoveFromSteal");
   }
     
   public static SwerveControllerCommand loadPathweaverTrajectory(String json) {
       Trajectory trajectory;
       try {
-        Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(json);
+        Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve("output/" + json + ".wpilib.json");
         trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
       } catch (IOException ex) {
         DriverStation.reportError("Unable to open trajectory: " + json, ex.getStackTrace());
@@ -83,5 +84,13 @@ public class AutoCommands {
       .andThen(new RotateToTarget().alongWith(new AlignHoodToTarget()).alongWith(new RampShooter(-3000)))
       .andThen(new ShootAuto(-3000).alongWith(new SetIndexerAuto(0.45, -3000)).alongWith(new RotateToTarget()).withTimeout(5))
       .andThen(moveOffInit);
+  }
+
+  public static Command steal5BallAuto()  {
+    return 
+      steal
+      .andThen(moveFromSteal)
+      .andThen(new RotateToTarget().alongWith(new AlignHoodToTarget()).alongWith(new RampShooter(-3000)))
+      .andThen(new ShootAuto(-3000).alongWith(new SetIndexerAuto(0.45, -3000)).alongWith(new RotateToTarget()).withTimeout(5));
   }
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -28,7 +28,6 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     robotContainer = new RobotContainer();
-
     AutoCommands.autoInit();
   }
 
